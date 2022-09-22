c94de9e427ccc9c661b38dae26807fbb6f513f27
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-22 17:58:24 -0500

    added about me section

---
+++ portfolio/src/components/About/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function About() {
+  return (
+    <section>
+      <h1 id='about'>
+        {' '}
+        I am a full stack web developer going through a coding bootcamp with the
+        University of Minnesota!
+      </h1>
+    </section>
+  );
+}
+
+export default About;
