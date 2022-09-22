b1cee9da198c95f91a43ad48009941055c517c01
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-22 18:46:37 -0500

    added photo

--- portfolio/src/components/About/index.js
+++ portfolio/src/components/About/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import coverImage from '../../assets/mexico.png';
 
 function About() {
   return (
-    <section>
+    <section className='my-5'>
+      <img
+        src={coverImage}
+        className='my-2'
+        style={{ width: '20%' }}
+        alt='cover'
+      />
       <h1 id='about'>
         {' '}
         I am a full stack web developer going through a coding bootcamp with the
         University of Minnesota!
       </h1>
     </section>
+    // <div>
+
+    // </div>
   );
 }
 
