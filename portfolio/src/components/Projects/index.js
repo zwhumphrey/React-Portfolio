f80c1dea841c1b3e9347b8c52d35209f7dd7e663
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-23 18:09:40 -0500

    added project file and photos

--- portfolio/src/components/Projects/index.js
+++ portfolio/src/components/Projects/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import photo from '../../assets';
 
 function projects() {
   const currentCategory = {
@@ -7,8 +9,15 @@ function projects() {
   };
   return (
     <section>
-      <h1>{currentCategory.name}</h1>
+      <h1>{capitalizeFirstLetter(currentCategory.name)}</h1>
       <p>{currentCategory.name}</p>
+      <div className='flex-row'>
+        <img
+          src={photo}
+          alt='Commercial Example'
+          className='img-thumbnail mx-1'
+        />
+      </div>
     </section>
   );
 }
