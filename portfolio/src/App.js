94cb4bdbe635b02bfa7d4ffac8e2c4197d690fcc
zwhumphrey <103855578+zwhumphrey@users.noreply.github.com>
2022-09-25 18:34:37 -0500

    added nav to home page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,9 +4,24 @@ import About from './components/About';
 import Projects from './components/Projects';
 
 function App() {
+  const [categories] = useState([
+    { name: 'About', description: 'Photo of me and my about section' },
+    { name: 'Contact', description: 'Infomation on where to contact me' },
+    {
+      name: 'Projects',
+      description: 'Links and photos of past projects',
+    },
+    { name: 'Resume', description: 'Link to resume' },
+  ]);
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
-      <Nav></Nav>
+      <Nav>
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      </Nav>
       <main>
         <Projects></Projects>
         <About></About>
